a45f59713338ef989cda3a1606a50c671ad050ea
Lorena Cavalcante de Oliveira <art0066448@aluno.firjansenaisesi.com.br>
2026-03-12 14:10:39 -0300

    segundo

--- controller/controller.js
+++ controller/controller.js
@@ -4,5 +4,14 @@ const app = express.Router()
 
 app.use(express.json())
 //rotas
+app.get("/usuario", () => {})
+
+app.get("/usuario/:id", () => {})
+
+app.post("/usuario", () => {})
+
+app.put("/usuario/:id", () => {})
+
+app.delete("/usuario/:id", () => {})
 
 module.exports = app
